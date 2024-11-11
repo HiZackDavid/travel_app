eeca06dcfa29ddfc6c39e44c87db94319d347016
Isaac Zolana <hizackdavid@hotmail.com>
2024-11-11 00:13:36 -0500

    Hero section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ interface Props {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
 }
 
 const Button = ({ type, title, icon, variant }: Props) => {
